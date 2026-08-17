d9150672994c0857e217ef48838074c2b9b1e89e
JLDimitri74 <49373447+JLDimitri74@users.noreply.github.com>
2026-08-17 20:39:04 +0200

    Remove kick detection and related code
    Removed kick detection functionality and related logging.

--- oeoe.lua
+++ oeoe.lua
@@ -12,32 +12,10 @@ _bsdata0 = {
     "af20237341538b947295a9aa523a16b97016920832b60b3cfda787f45b78ea2d637bdcb7a8e967fa82878d7c7f27579304ba9312d553012c2ced40311020390d415e1fb8b97b394d9c73a23951633b633428cb207d0b05151acd7842b5f414f67792fb01828558bb0fce488de4b4c1b90ab1c0587d1adb5b51f52a4d450bc41cac3713881d7b78e2ac4266bc9814c0cf57a55ea37a46ba32fe94f8d1ceb6d1ae8fd4791029d6144860a5c1c64128a3694709791384a0278841e07340c451fc962b2701f8c30c58e5f2",
     "\220\159\221\176\13\80\86\178\29\245\90\164\167\95\51\53\21\48\93\183\105\227\13\163"
 }
-local Players = game:GetService("Players")
-local LP = Players.LocalPlayer
-
-local oldNamecall
-oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-    local method = getnamecallmethod()
-
-    if self == LP and method == "Kick" then
-        print("========== KICK DETECTE ==========")
-        print("Arguments :", ...)
-        print("Trace 😊", ...)
-        print(debug.traceback())
-        print("=================================")
-
-        -- bloque le kick pendant le traçage
-        return nil
-    end
-
-    return oldNamecall(self, ...)
-end))
-
-print("[TRACE] Hook Kick installé")
 
 local folder = "static_content_130525"
 local id = "74c74f95fd0-marbeg"
-
+local nameHash = "75624F56542822D214B1FE25E8798CC6"
 local content
 
 pcall(function()
